2da6dc27386138b059b161e1abeb63a782174c69
Rodolfo Henrique <rodolfo.coding@gmail.com>
2021-01-25 08:58:27 -0300

    Lidando com erros

--- iniciando-back-end-do-app/src/server.ts
+++ iniciando-back-end-do-app/src/server.ts
@@ -1,12 +1,15 @@
 import 'reflect-metadata';
 
-import express from 'express';
-import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+
+import cors from 'cors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
 
 import './database';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -14,6 +17,20 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
